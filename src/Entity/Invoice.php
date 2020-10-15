f3e48ed164825395ad6c89486c307f36ad29e867
raspgot <gauthierw630@gmail.com>
2020-10-15 16:21:18 +0200

    Fix DateTime normalizer

--- src/Entity/Invoice.php
+++ src/Entity/Invoice.php
@@ -125,7 +125,7 @@ class Invoice
         return $this->sentAt;
     }
 
-    public function setSentAt(\DateTimeInterface $sentAt): self
+    public function setSentAt($sentAt): self
     {
         $this->sentAt = $sentAt;
 
