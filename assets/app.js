6f66233c049dfe4bd83e36a1944170d6e370c1a4
raspgot <gauthierw630@gmail.com>
2020-10-16 15:55:42 +0200

    Install React 16.8.6

--- assets/app.js
+++ assets/app.js
@@ -5,10 +5,21 @@
  * (and its CSS file) in your base layout (base.html.twig).
  */
 
+// Imports
+import React from 'react';
+import ReactDOM from "react-dom";
+
 // any CSS you import will output into a single css file (app.css in this case)
 import './styles/app.css';
 
 // Need jQuery? Install it with "yarn add jquery", then uncomment to import it.
 // import $ from 'jquery';
 
-console.log('Hello Webpack Encore! Edit me in assets/app.js');
+console.log('Hello Webpack Encore !!! Edit me in assets/app.js');
+
+const App = () => {
+    return <h1>Bonjour !</h1>
+}
+
+const rootElement = document.querySelector('#app');
+ReactDOM.render(<App />, rootElement);
